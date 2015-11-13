cec09575a354b62a42d826b28aeefc2f01386c8e
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-11-13 11:25:01 +0100

    Create install-and-run-azurevolumedriver.sh

---
+++ install-and-run-azurevolumedriver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Call this script with account name and storage key parameters
+# sh scriptname name key
+
+echo "Setting up Volume Driver"
+
+git clone https://github.com/s-innovations/azurefile-dockervolumedriver.git
+./azurefile-dockervolumedriver/azurefile-dockervolumedriver --account-name $1 --account-key $2 &
+
+exit 0
