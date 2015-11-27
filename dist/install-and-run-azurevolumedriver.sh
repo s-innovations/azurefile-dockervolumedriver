bac4412443e462ce939f7124c1b1c430e649bb13
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-11-27 01:10:26 +0100

    updated to use go in the correct way

--- dist/install-and-run-azurevolumedriver.sh
+++ dist/install-and-run-azurevolumedriver.sh
@@ -4,9 +4,22 @@
 
 echo "Setting up Volume Driver"
 echo "Connection Info"
-echo $1
-echo $2
-git clone https://github.com/s-innovations/azurefile-dockervolumedriver.git
-sudo ./azurefile-dockervolumedriver/dist/azurefile-dockervolumedriver --account-name $1 --account-key $2 &
+#echo $1
+#echo $2
+
+
+sudo wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.5.linux-amd64.tar.gz
+
+sudo export GOROOT=/usr/local/go
+sudo export PATH=$PATH:$GOROOT/bin
+
+sudo git clone https://github.com/s-innovations/azurefile-dockervolumedriver.git src/azurefile
+sudo export GOPATH=`pwd`
+sudo export GO15VENDOREXPERIMENT=1
+sudo cd src/azurefile
+sudo go build -o azurefile
+
+sudo ./azurefile --account-name $1 --account-key $2 &
 
 exit 0
