2df641e40368fe37e80593603592efdf2c5e54bc
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-11-27 19:50:59 +0100

    remove sudo

--- dist/install-and-run-azurevolumedriver.sh
+++ dist/install-and-run-azurevolumedriver.sh
@@ -8,18 +8,18 @@ echo "Connection Info"
 #echo $2
 
 
-sudo wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.5.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.5.linux-amd64.tar.gz
 
-sudo export GOROOT=/usr/local/go
-sudo export PATH=$PATH:$GOROOT/bin
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
 
-sudo git clone https://github.com/s-innovations/azurefile-dockervolumedriver.git src/azurefile
-sudo export GOPATH=`pwd`
-sudo export GO15VENDOREXPERIMENT=1
-sudo cd src/azurefile
-sudo go build -o azurefile
+git clone https://github.com/s-innovations/azurefile-dockervolumedriver.git src/azurefile
+export GOPATH=`pwd`
+export GO15VENDOREXPERIMENT=1
+cd src/azurefile
+go build -o azurefile
 
-sudo ./azurefile --account-name $1 --account-key $2 &
+./azurefile --account-name $1 --account-key $2 &
 
 exit 0
